a11d74c3f0d7a1eb0889494b2acb59841906f944
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-03-07 11:42:20 +0600

    Add files via upload

---
+++ Python/Matplotlib/add_text.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5]
+y=[3,1,5,2,6]
+
+plt.plot(x,y)
+plt.title("CIT-220",fontsize=15)
+plt.xlabel("Days",fontsize=15)
+plt.ylabel("Python",fontsize=15)
+
+plt.text(2,5,"Java",fontsize=15,style="italic",bbox={"facecolor":"red"})
+
+
+plt.annotate("python",xy=(1,3),xytext=(4,4),arrowprops=dict(facecolor="black",shrink=100))
+plt.show()

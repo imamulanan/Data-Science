8371a230a7d782bd2b62cb880e82fe7a0e43466c
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-06-03 01:14:46 +0600

    Add files via upload

---
+++ Python/Module_1_2/python_range.py
@@ -0,0 +1,12 @@
+a=range(90)
+print(a)
+
+b=list(range(20)) #full list dibe
+print(b)
+
+# range(start,end,sequence)
+c=list(range(1,20,2)) # amake value dibe 20-1 porjnto
+print(c)
+
+d=list(range(100,2,-2))
+print(d)

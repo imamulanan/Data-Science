49a5542f6031aed33a4ebee3d513b864ac3eba20
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-06-03 12:09:19 +0600

    Update first.py

--- Python/Numpy/first.py
+++ Python/Numpy/first.py
@@ -7,3 +7,9 @@ print(type(my_lst))
 print(arr1)
 print(arr1.ndim) # ndim diye bujhaay n dymansional
 print(arr1.shape)
+
+lst1=[1,2,3,4,5]
+lst2=[3,4,5,6,7]
+lst3=[6,5,4,3,2]
+
+arr2=np.array([lst1,lst2,lst3])

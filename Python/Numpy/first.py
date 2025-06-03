e26a4568cc316d02f1cd207d004afb6f264f7e40
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-06-03 12:08:31 +0600

    Update first.py

--- Python/Numpy/first.py
+++ Python/Numpy/first.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+my_lst=[1,2,3,4,5]
+arr1=np.array(my_lst)
+print(type(arr1))
+print(type(my_lst))
+print(arr1)
+print(arr1.ndim) # ndim diye bujhaay n dymansional
+print(arr1.shape)

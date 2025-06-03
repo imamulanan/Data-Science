cbb7be7e4823211fb838f5513ac9690839e84373
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-06-03 12:10:08 +0600

    Update first.py

--- Python/Numpy/first.py
+++ Python/Numpy/first.py
@@ -13,3 +13,12 @@ lst2=[3,4,5,6,7]
 lst3=[6,5,4,3,2]
 
 arr2=np.array([lst1,lst2,lst3])
+print(arr2)
+print(arr2.ndim)
+
+
+import timeit
+
+code = "[j**4 for j in range(1,9)]"
+execution_time = timeit.timeit(code, number=10000)
+print(f"Execution time: {execution_time:.6f} seconds")

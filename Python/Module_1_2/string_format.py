570176089d530f36ee91de781ff8a611fa75bf0a
imamulanan <anan.imamul@gmail.com>
2025-06-05 01:13:44 +0600

    initial commit

---
+++ Python/Module_1_2/string_format.py
@@ -0,0 +1,11 @@
+# user_input=input("What is your name?")
+# a="Good Morning, {}. How are you? {}".format(user_input,"Hi")
+# print(user_input)
+# print(a)
+
+age=25
+f_name="Imamul Kabir"
+l_name="Anan"
+
+txt=f"I am {f_name} {l_name}. I am {age} years old." # f use korle auto niye nibe
+print(txt)

068438242873a1e4e0c46f09aeb1972408e6373d
imamulanan <anan.imamul@gmail.com>
2025-06-04 20:45:47 +0600

    updated file

--- Python/Module_1_2/set_python.py
+++ Python/Module_1_2/set_python.py
@@ -7,3 +7,16 @@
 a=[1,2,2,2,3,4,4,4,5]
 s=set(a)
 print(s)
+# print(s[0]) # indexing koreow value ber kora jabe nah 
+
+
+#union intersection 
+
+b={1,2,3,4,5,6}
+c={3,4,5,6,7,8}
+
+d=b.intersection(c)
+print(d)
+
+e=b.union(c)
+print(e)

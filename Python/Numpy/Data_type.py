d01ad15a2ceeffa0bcfe99b691ac84005594fc24
imamulanan <anan.imamul@gmail.com>
2025-06-22 20:28:04 +0600

    Updated commit

--- Python/Numpy/Data_type.py
+++ Python/Numpy/Data_type.py
@@ -7,3 +7,11 @@ print("Data types: ",var.dtype)
 var1 = np.array([1.0,1.5,2.6])
 
 print("Data types: ",var1.dtype)
+
+var2 = np.array(['a','b','c'])
+
+print("Data types: ",var2.dtype)
+
+var3 = np.array([1,2,3,4,5,6,7],dtype=np.int64)
+
+print("Data types: ",var3.dtype)

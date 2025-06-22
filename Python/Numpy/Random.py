890547cedf0ac650fc85655416687735432e848c
imamulanan <anan.imamul@gmail.com>
2025-06-22 19:35:51 +0600

    Updated commit

--- Python/Numpy/Random.py
+++ Python/Numpy/Random.py
@@ -5,3 +5,19 @@ import numpy as np
 var = np.random.rand(4) # amake 1d array te 4 ta element dibe and ta 0-1 er vitor hobe
 
 print(var)
+
+var1 = np.random.rand(2,5)
+
+print(var1)
+
+# randn()-->
+
+var2 = np.random.randn(5) # etar khetre positive negative 2 o ta hote pare
+
+print(var2)
+
+# ranf()-->
+
+var3 = np.random.ranf(4)
+
+print(var3)

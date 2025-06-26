3ba5ac3f3e6b75595154e4acbb8ba18405b156b2
imamulanan <anan.imamul@gmail.com>
2025-06-26 21:19:38 +0600

    updated

---
+++ Python/Numpy/Array.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# x = [1,2,3,4]
+# y = np.array(x)
+# z = np.array([4,5,6,7,8])
+
+# print(y)
+# print(z)
+
+# print(type(y))
+# print(type(z))
+
+# 2nd code
+
+l = []
+
+for i in range(1,5):
+    int_1 = input("enter: ")
+    l.append(int_1)
+
+print(np.array(l))    
